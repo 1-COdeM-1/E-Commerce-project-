8cf9863e8580d755948c76f40cb689a785592392
1-COdeM-1 <obitachi3840@gmail.com>
2026-08-27 03:54:57 +0300

    fix: update getCategories function to remove unused request body parameter

--- BackEnd/src/controllers/productController.ts
+++ BackEnd/src/controllers/productController.ts
@@ -14,9 +14,9 @@ const listProducts = async(req : Request , res : Response , next : NextFunction 
         next(e)
     }
 }
-const getCategories = async(req : Request, res : Response , next : NextFunction )=>{
+const getCategories = async(_req : Request, res : Response , next : NextFunction )=>{
     try{
-        const body = req.body
+        
         const activeOnly = eq(products.active , true );
         const result = await db.select({category:products.category}).from(products).where(activeOnly) ;
         const categories = [...new Set(result.map(r=>r.category).sort((a , b)=>a.localeCompare(b)))]
