ddff00ae1ea4a8425c56f76a40faf13bf0e51b56
1-COdeM-1 <obitachi3840@gmail.com>
2026-09-13 21:32:03 +0300

    feat: add CheckoutReturnPage component for order confirmation and cart clearing logic

--- FrontEnd/src/App.jsx
+++ FrontEnd/src/App.jsx
@@ -6,6 +6,7 @@ import { Routes  , Route, Navigate} from "react-router";
 import HomePage from "./pages/homePage";
 import CartPage from "./pages/CartPage";
 import OrdersPage from "./pages/OrdersPage";
+import CheckoutReturnPage from "./pages/CheckoutReturnPage";
 function App() {
   const {isLoaded , isSignedIn} = useAuth() ;
   if(!isLoaded) return <PageLoader />
@@ -15,7 +16,7 @@ function App() {
         <Route path = "/" element = {<HomePage />} />
         <Route path = "/cart" element = {<CartPage />} />
         <Route path = "/orders" element = {isSignedIn ? <OrdersPage /> : <Navigate to={"/"} replace/>} />
-      </Routes>
+        <Route path="/checkout/return" element={<CheckoutReturnPage />} />      </Routes>
     </Layout>
   );
 }
